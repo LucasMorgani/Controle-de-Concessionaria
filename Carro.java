0b20a766a9594174d5cc5a55a494b24f9fd9fd09
Lucas Gomes Morgani <lucasmorgani@hotmail.com>
2022-07-04 17:25:58 -0300

    Removendo coisas desnecessárias

--- Carro.java
+++ Carro.java
@@ -1,12 +1,3 @@
-/* 
-GRUPO 7
-Lucas Gomes Morgani da Silva    26529041	
-Juliano Belentani               25972502	
-Leonardo Ray Azevedo Bertuliano 26470811	
-Samuel Vinícius Basílio         26414911	
-Diego Ribeiro Duarte            28027639
-Fábio Luiz Teixeira             28031270	
-*/
 public class Carro extends Veiculo{
 
     
